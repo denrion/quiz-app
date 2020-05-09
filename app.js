0ffeaa6d683f079a8ef8659f44a8f61eb29add41
Nemanja <kuzmaneca@gmail.com>
2020-05-09 11:49:06 +0200

    add security middleware

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const compression = require('compression');
+const mongoSanitize = require('express-mongo-sanitize');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const xss = require('xss-clean');
 
 const globalErrorHandler = require('./controllers/errorController');
 const NotImplementedError = require('./utils/errors/AppError');
@@ -14,8 +20,37 @@ const app = express(express.json({ extended: false }));
 
 // 1) GLOBAL MIDDLEWARES
 
+// SET security HTTP headers
+app.use(helmet());
+
 // Body Parser, reading data from body into req.body
 app.use(express.json({ limit: process.env.BODY_PARSER_SIZE_LIMIT }));
+
+// Rate limiting - for stopping BRUTE FORCE attacks from same IP
+const limiter = rateLimit({
+  max: process.env.RATE_LIMIT_MAX_NUM_CONNECTIONS,
+  windowMs: process.env.RATE_LIMIT_KEEP_IN_MEMORY_LENGTH_MS,
+  message: process.env.RATE_LIMIT_MESSAGE,
+});
+
+app.use('/api', limiter);
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization agains XSS
+app.use(xss());
+
+// Prevent http parameter polution
+// specify parameters that are allowed to be repeated
+app.use(
+  hpp({
+    whitelist: [],
+  })
+);
+
+// Compress API data
+app.use(compression());
 
 // CORS
 app.use(cors());
